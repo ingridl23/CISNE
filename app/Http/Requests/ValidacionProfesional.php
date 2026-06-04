defb14377985cbe8afd9d9405b5d2f3c85ac3979
ingrid ledesma <ingridledesma22@gmail.com>
2026-06-04 20:58:31 -0300

    Fix class name casing for ValidacionProfesional

--- app/Http/Requests/ValidacionProfesional.php
+++ app/Http/Requests/ValidacionProfesional.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class validacionProfesional extends FormRequest
+class ValidacionProfesional extends FormRequest
 {
     /**
      * Autorizar el uso del FormRequest.
